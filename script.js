21a0eacae22309199b33e9e4386e721f1b937658
Sarah Nosal <nosalsm@gmail.com>
2021-11-17 11:19:37 -0600

    added tabs

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const h2 = document.createElement("h2");
 h2.textContent = "A Little About Me";
 document.querySelector("body").appendChild(h2);
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
